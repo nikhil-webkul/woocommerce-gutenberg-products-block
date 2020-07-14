2e369e017ed6671e50eab1a31a13778cc8544675
Rua Haszard <rua.haszard@automattic.com>
2020-07-14 15:01:33 +1200

    only show 'create account' checkbox when appropriate (guest checkout)

--- assets/js/blocks/cart-checkout/checkout/block.js
+++ assets/js/blocks/cart-checkout/checkout/block.js
@@ -183,7 +183,7 @@ const Checkout = ( { attributes, scrollToTop } ) => {
 		window.location.href
 	) }`;
 
-	if ( ! isEditor && ! customerId && ! CHECKOUT_ALLOWS_GUEST ) {
+	if ( ! isEditor && ! customerId && ! CHECKOUT_ALLOWS_GUEST && ! CHECKOUT_ALLOWS_SIGNUP ) {
 		return (
 			<>
 				{ __(
@@ -199,6 +199,15 @@ const Checkout = ( { attributes, scrollToTop } ) => {
 			</>
 		);
 	}
+
+	const createAccountUI = ! customerId && CHECKOUT_ALLOWS_SIGNUP && (
+		<CheckboxControl
+			className="wc-block-checkout__create-account"
+			label={ __( 'Create an account?', 'woo-gutenberg-products-block' ) }
+			checked={ createAccount }
+			onChange={ ( value ) => setCreateAccount( value ) }
+		/>
+	);
 
 	const loginPrompt = () =>
 		CHECKOUT_SHOW_LOGIN_REMINDER &&
@@ -246,19 +255,7 @@ const Checkout = ( { attributes, scrollToTop } ) => {
 								onChange={ setEmail }
 								required={ true }
 							/>
-							{ CHECKOUT_ALLOWS_SIGNUP && (
-								<CheckboxControl
-									className="wc-block-checkout__create-account"
-									label={ __(
-										'Create an account?',
-										'woo-gutenberg-products-block'
-									) }
-									checked={ createAccount }
-									onChange={ ( value ) =>
-										setCreateAccount( value )
-									}
-								/>
-							) }
+							{ createAccountUI }
 						</FormStep>
 						{ needsShipping && (
 							<FormStep
